471eba4f52f37d4bc026ed07f90781316ad52da0
barbvilla <barbara.villasenor.c@gmail.com>
2023-06-07 18:02:18 -0400

    error de campos vacios OK

--- bq-project/src/components/Login.jsx
+++ bq-project/src/components/Login.jsx
@@ -1,28 +1,73 @@
 import { useState } from "react";
+import './Login.css'
 
 export default function LoginSection() {
-  const [userEmail, setUserEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [userEmail, setUserEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState("");
+  const [passError, setPassError] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (!password || !userEmail) {
+      setPassError("Por favor, escribe una contraseña") || setEmailError("Por favor, escribe un email");
+      return;
+    }
+
+    if (!userEmail) {
+      setEmailError("Por favor, escribe un email");
+      return;
+    }
+
+    if (!password) {
+      setPassError("Por favor, escribe una contraseña");
+      return;
+    }
+
     fetch('http://localhost:8080/login', {
       method: 'POST',
-      body: JSON.stringify({email: userEmail, password: password}),
-      headers:{
+      body: JSON.stringify({ email: userEmail, password: password }),
+      headers: {
         'Content-Type': 'application/json'
-      } 
+      }
     })
       .then(res => res.json())
-      .catch(error => console.log('Error:', error))
+      .catch(err => console.log('Error:', err))
       .then(response => console.log('Success:', response));
   }
 
   return (
-    <form className="loginForm" onSubmit={handleSubmit}>
-      <input placeholder='Escribe tu correo' type='email' onChange={e => setUserEmail(e.target.value)} value={userEmail} />
-      <input placeholder='Escribe tu contraseña' type='password' onChange={e => setPassword(e.target.value)} value={password} />
-      <button>Iniciar Sesión</button>
-    </form>
+    <>
+      <section className="a"></section>
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <h1>Bienvenido</h1>
+        <input
+          placeholder='Escribe tu correo'
+          type='email'
+          onChange={e => setUserEmail(e.target.value)}
+          value={userEmail}
+        />
+        {emailError && (
+          <p1 className="error">
+            <br />
+            {emailError}
+          </p1>
+        )}
+        <input
+          placeholder='Escribe tu contraseña'
+          type='password'
+          onChange={e => setPassword(e.target.value)}
+          value={password}
+        />
+        {passError && (
+          <p1 className="error">
+            <br />
+            {passError}
+          </p1>
+        )}
+        <button>Iniciar Sesión</button>
+      </form>
+    </>
   );
 }
